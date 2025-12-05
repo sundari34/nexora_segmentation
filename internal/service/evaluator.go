be0f7f853a3af2c2970a853999b4ced767a4f4dc
VishnuCris <vishnunmms@gmail.com>
2025-12-05 20:39:41 +0530

    work related to property and channel reachability completed

--- internal/service/evaluator.go
+++ internal/service/evaluator.go
@@ -260,14 +260,33 @@ func prefilterCandidates(req models.SegmentPayload) ([]string, error) {
 		finalHaving = "HAVING " + strings.Join(havingClauses, " AND ")
 	}
 
+	fmt.Println(req)
+	fmt.Println("((((((((((((((((((((((req))))))))))))))))))))))")
+	// check user property
+	userPropertySql := ""
+	if req.Property != "" {
+		userPropertySql = fmt.Sprintf(", COALESCE(JSON_UNQUOTE(JSON_EXTRACT(user_properties, '$.%s')), 'default') AS property", req.Property)
+	}
+
+	if req.Channel != "" {
+		if req.Channel == "email" {
+			finalWhere += " AND email is not NULL AND email != ''"
+		} else if req.Channel == "mobile" || req.Channel == "sms" {
+			finalWhere += " AND mobile is not NULL AND mobile != ''"
+		} else if req.Channel == "push" || req.Channel == "web_push" {
+			finalWhere += " AND id in (select external_user_id from notification_tokens where token is not null and token != '')"
+		}
+	}
+
 	q := fmt.Sprintf(`
-		SELECT nexora_id
+		SELECT nexora_id %s
 		FROM event_daily
 		WHERE %s
 		GROUP BY nexora_id
 		%s
-	`, finalWhere, finalHaving)
-
+	`, userPropertySql, finalWhere, finalHaving)
+	fmt.Println(q)
+	fmt.Println("FInal Query ------------------------------------")
 	if db.IsQueryLoggingEnabled() {
 		log.Printf("[ClickHouse] Prefilter Query: %s | Params: %+v %+v\n", q, params, havingParams)
 	}
@@ -523,7 +542,8 @@ func deepFilter(req models.SegmentPayload, nexoraIDs []string) ([]models.Member,
 	if f.ConditionBlock != nil {
 		cond = f.ConditionBlock.Condition
 	}
-
+	fmt.Println(req)
+	fmt.Println("((((((((((((((((((((((req))))))))))))))))))))))")
 	// check user property
 	userPropertySql := ""
 	if req.Property != "" {
