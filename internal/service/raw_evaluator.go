6676c2f5f86c3ceb12d19b68cec1cbe4fa6c7913
VishnuCris <vishnunmms@gmail.com>
2026-01-30 16:50:06 +0530

    raw sql geenrated

--- internal/service/raw_evaluator.go
+++ internal/service/raw_evaluator.go
@@ -367,6 +367,8 @@ func EvaluteRaw(req models.SegmentNewPayload) ([]models.Member, error) {
 		finalHaving = "HAVING " + strings.Join(groupHaving, " "+groupCondition+" ")
 	}
 
+	fmt.Println(groupWhere)
+	fmt.Println(groupHaving)
 	fmt.Println(len(groupWhere))
 	fmt.Println(len(groupHaving))
 	fmt.Println(len(groupWhere) > 0)
