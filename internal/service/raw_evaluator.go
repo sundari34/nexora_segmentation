bd5b6e828679a06c7c854db3f6d5d7ffb552fbb2
VishnuCris <vishnunmms@gmail.com>
2026-02-03 18:31:51 +0530

    raw sql geenrated

--- internal/service/raw_evaluator.go
+++ internal/service/raw_evaluator.go
@@ -458,8 +458,6 @@ func EvaluteRaw(req models.SegmentNewPayload) (map[string]interface{}, error) {
 		whereNonAggregateStatement = buildInCondition("np.nexora_id", req.NexoraIDs)
 	}
 
-	return nil, fmt.Errorf("checking ----------")
-
 	selectStatement := "SELECT cp.id AS customer_profile_id, any(np.nexora_id) AS nexora_id, JSONExtractString(argMaxMerge(cp.user_properties_state), 'gender') AS gender"
 	if req.Source == "campaign_service" {
 		selectStatement = fmt.Sprintf("SELECT cp.id AS customer_profile_id, any(np.nexora_id) AS nexora_id, coalesce( nullIf(JSONExtractString(argMaxMerge(cp.user_properties_state), '%s'), ''), 'default') AS property", req.Property)
