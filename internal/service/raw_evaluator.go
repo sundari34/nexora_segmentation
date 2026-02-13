1930405c8fa9cf8cbabf2a76d49e8125da552071
VishnuCris <vishnunmms@gmail.com>
2026-02-13 17:20:04 +0530

    added refined go.mod and sum

--- internal/service/raw_evaluator.go
+++ internal/service/raw_evaluator.go
@@ -603,13 +603,13 @@ func EvaluteRaw(req models.SegmentNewPayload) (map[string]interface{}, error) {
 		finalWhere = "WHERE " + strings.Join(groupWhere, " "+groupCondition+" ")
 		withStatement = fmt.Sprintf("WITH event_users AS (SELECT DISTINCT ev.nexora_id FROM events ev INNER JOIN event_daily ed ON ev.event_name = ed.event_name AND ev.nexora_id = ed.nexora_id %s)", finalWhere)
 		joinStatement = "event_users eu INNER JOIN cp_resolved cp ON eu.nexora_id = cp.nexora_id"
-		overallSelectStatement = fmt.Sprintf("%s, cp_resolved AS (%s from customer_profiles_latest cp %s group by cp.id %s) select cp.nexora_id as nexora_id, cp.customer_profile_id, cp.external_user_id, cp.email, cp.mobile, cp.name, cp.project_id, cp.client_id, cp.user_properties, cp.property from %s order by customer_profile_id %s", withStatement, selectStatement, whereNonAggregateStatement, finalHaving, joinStatement, limitAndOffsets)
-		countStatement = fmt.Sprintf("%s, cp_resolved AS (%s from customer_profiles_latest cp %s group by cp.id %s) select COUNT(*) AS total_count FROM %s %s", withStatement, selectStatement, whereNonAggregateStatement, finalHaving, joinStatement, limitAndOffsets)
+		overallSelectStatement = fmt.Sprintf("%s, cp_resolved AS (%s from customer_profiles_latest cp %s group by cp.id, cp.external_user_id %s) select cp.nexora_id as nexora_id, cp.customer_profile_id, cp.external_user_id, cp.email, cp.mobile, cp.name, cp.project_id, cp.client_id, cp.user_properties, cp.property from %s order by customer_profile_id %s", withStatement, selectStatement, whereNonAggregateStatement, finalHaving, joinStatement, limitAndOffsets)
+		countStatement = fmt.Sprintf("%s, cp_resolved AS (%s from customer_profiles_latest cp %s group by cp.id, cp.external_user_id %s) select COUNT(*) AS total_count FROM %s %s", withStatement, selectStatement, whereNonAggregateStatement, finalHaving, joinStatement, limitAndOffsets)
 
 	} else {
 		joinStatement = "customer_profiles_latest cp"
-		overallSelectStatement = fmt.Sprintf("%s from %s %s group by cp.id %s order by customer_profile_id %s", selectStatement, joinStatement, whereNonAggregateStatement, finalHaving, limitAndOffsets)
-		countStatement = fmt.Sprintf("SELECT COUNT(*) AS total_count FROM ( %s from %s %s group by cp.id %s %s) as sub", selectStatement, joinStatement, whereNonAggregateStatement, finalHaving, limitAndOffsets)
+		overallSelectStatement = fmt.Sprintf("%s from %s %s group by cp.id, cp.external_user_id %s order by customer_profile_id %s", selectStatement, joinStatement, whereNonAggregateStatement, finalHaving, limitAndOffsets)
+		countStatement = fmt.Sprintf("SELECT COUNT(*) AS total_count FROM ( %s from %s %s group by cp.id, cp.external_user_id %s %s) as sub", selectStatement, joinStatement, whereNonAggregateStatement, finalHaving, limitAndOffsets)
 	}
 
 	var count uint64
